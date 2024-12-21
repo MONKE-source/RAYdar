7c35f721534c147518a20056c09b322baa556ced
Goh Min Wen, Ted <ilovefirebase4@gmail.com>
2024-12-21 13:41:22 +0800

    working up till now

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/test.tsx",
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  output: { filename: "index.js" },
+};
